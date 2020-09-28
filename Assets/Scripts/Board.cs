484fc99c9b5e1d1485012ee5532bc9fd8f77eddf
honnage <hon_da-1998@hotmail.com>
2020-09-29 03:21:53 +0700

    Analyze

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+    private BackgroundTile[,] allTiles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allTiles = new BackgroundTile[width, height];
+        SetUp();
+    }
+
+    //สร้างตารางโดยลูปตามช่องที่ได้กำหนดไว้ กว้าง x สูง
+    private void SetUp()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                Vector2 tempPosition = new Vector2(i, j);
+                Instantiate(tilePrefab, tempPosition, Quaternion.identity);
+            }
+        }
+    }
+}

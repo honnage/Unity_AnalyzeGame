6d217eeb6c077fd6469d905477876f0c83d847ea
honnage <hon_da-1998@hotmail.com>
2020-09-29 23:53:12 +0700

    Using the mouse to find the angle of degrees

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -8,12 +8,15 @@ public class Board : MonoBehaviour
     public int width;
     public int height;
     public GameObject tilePrefab;
+    public GameObject[] dots;
     private BackgroundTile[,] allTiles;
+    public GameObject[,] allDots;
 
     // Start is called before the first frame update
     void Start()
     {
         allTiles = new BackgroundTile[width, height];
+        allDots = new GameObject[width, height];
         SetUp();
     }
 
@@ -28,6 +31,12 @@ public class Board : MonoBehaviour
                 GameObject backgroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity) as GameObject;
                 backgroundTile.transform.parent = this.transform;
                 backgroundTile.name = "( " + i + " , " + j + " )";
+
+                int dotToUse = Random.Range(0, dots.Length);
+                GameObject dot = Instantiate(dots[dotToUse], tempPosition, Quaternion.identity);
+                dot.transform.parent = this.transform;
+                dot.name = "( " + i + " , " + j + " )";
+                allDots[i, j] = dot;
             }
         }
     }

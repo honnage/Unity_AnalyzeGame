6317d579b736b680fa1040ab1ff24ad07e370e8f
honnage <hon_da-1998@hotmail.com>
2020-10-03 00:31:43 +0700

    Collapsing Columns

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,6 +1,7 @@
 ﻿
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 
 public class Board : MonoBehaviour
@@ -108,5 +109,28 @@ public class Board : MonoBehaviour
                 }
             }
         }
+        StartCoroutine(DecreseRowCo());
+    }
+
+    private IEnumerator DecreseRowCo()
+    {
+        int nullCount = 0;
+        for(int i = 0; i< width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if(allDots[i, j] == null)
+                {
+                    nullCount++;
+                }
+                else if(nullCount > 0)
+                {
+                    allDots[i, j].GetComponent<Dot>().row -= nullCount;
+                    allDots[i, j] = null;
+                }
+            }
+            nullCount = 0;
+        }
+        yield return new WaitForSeconds(.4f);
     }
 }
--- Assets/Scripts/Dot.cs
+++ Assets/Scripts/Dot.cs
@@ -48,28 +48,34 @@ public class Dot : MonoBehaviour
         {
             //Move Towards the target
             tempPosition = new Vector2(targetX, transform.position.y);
-            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .6f);
+            if(board.allDots[column, row] != this.gameObject)
+            {
+                board.allDots[column, row] = this.gameObject;
+            }
         }
         else
         {
             //Directly set the position 
             tempPosition = new Vector2(targetX, transform.position.y);
             transform.position = tempPosition;
-            board.allDots[column, row] = this.gameObject;
         }
 
         if (Mathf.Abs(targetY - transform.position.y) > .1)
         {
             //Move Towards the target
             tempPosition = new Vector2(transform.position.x, targetY);
-            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .6f);
+            if (board.allDots[column, row] != this.gameObject)
+            {
+                board.allDots[column, row] = this.gameObject;
+            }
         }
         else
         {
             //Directly set the position 
             tempPosition = new Vector2(transform.position.x, targetY);
             transform.position = tempPosition;
-            board.allDots[column, row] = this.gameObject;
         }
     }
 

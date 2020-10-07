d096a7479b9999ca44f2fd8947f5eb94eed8dc32
honnage <hon_da-1998@hotmail.com>
2020-10-08 01:01:31 +0700

    Breakable Jelly tiles

--- Assets/Scripts/BackgroundTile.cs
+++ Assets/Scripts/BackgroundTile.cs
@@ -5,18 +5,34 @@ using UnityEngine;
 public class BackgroundTile : MonoBehaviour
 {
     public int hitPoints;
+    private SpriteRenderer sprite;
 
-    private void Update()
+    private void Start()
     {
-        if (hitPoints <= 0)
-        {
-            Destroy(this.gameObject);
-        }
+        sprite = GetComponent<SpriteRenderer>();
     }
 
-    public void TakeDamage(int damage)
-    {
-        hitPoints -= damage;
-    }
+	private void Update()
+	{
+		if (hitPoints <= 0)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+
+	public void TakeDamage(int damage)
+	{
+		hitPoints -= damage;
+		MakeLighter();
+	}
+
+	void MakeLighter()
+	{
+		//take the current color
+		Color color = sprite.color;
+		//Get the current color's alpha value and cut it in half.  
+		float newAlpha = color.a * .5f;
+		sprite.color = new Color(color.r, color.g, color.b, newAlpha);
+	}
 
 }
--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -414,6 +414,8 @@ public class Board : MonoBehaviour
         findMatches.currentMatches.Clear();
         currentDot = null;
         yield return new WaitForSeconds(.5f);
+
+
         currentState = GameState.move;
 
     }

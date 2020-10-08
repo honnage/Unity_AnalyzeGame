9b9acff3c0905d0405dd3eaad089221b89b84451
honnage <hon_da-1998@hotmail.com>
2020-10-08 23:09:53 +0700

    Update Background and icon

--- Assets/Scripts/CameraScalar.cs
+++ Assets/Scripts/CameraScalar.cs
@@ -9,22 +9,23 @@ public class CameraScalar : MonoBehaviour
     public float cameraOffset;
     public float aspectRatio = 0.625f;
     public float padding = 2;
+    public float yOffset = 1;
 
-    // Start is called before the first frame update
+    // Use this for initialization
     void Start()
     {
         board = FindObjectOfType<Board>();
-        if(board != null)
+        if (board != null)
         {
-            RepoitionCamera(board.width - 1, board.height - 1);
+            RepositionCamera(board.width - 1, board.height - 1);
         }
     }
 
-    void RepoitionCamera(float x, float y)
+    void RepositionCamera(float x, float y)
     {
-        Vector3 tempPosition = new Vector3( x / 2, y / 2, cameraOffset);
+        Vector3 tempPosition = new Vector3(x / 2, y / 2 + yOffset, cameraOffset);
         transform.position = tempPosition;
-        if (board.width > board.height)
+        if (board.width >= board.height)
         {
             Camera.main.orthographicSize = (board.width / 2 + padding) / aspectRatio;
         }

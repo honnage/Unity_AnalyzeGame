6bbd6922a435c4fb9cb0e3f1a3a51a34b2795329
honnage <hon_da-1998@hotmail.com>
2020-11-06 18:58:51 +0700

    Fixing Bomb finding:

--- Assets/Scripts/Game Data  Scripts/GameData.cs
+++ Assets/Scripts/Game Data  Scripts/GameData.cs
@@ -71,6 +71,20 @@ public class GameData : MonoBehaviour
             file.Close();
             Debug.Log("Loaded");
         }
+        else
+        {
+            saveData = new SaveData();
+            saveData.isActive = new bool[100];
+            saveData.stars = new int[100];
+            saveData.highScores = new int[100];
+            saveData.isActive[0] = true;
+
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        Save();
     }
 
    private void OnDisable()
